bfb7c5f56f589d60c081a95abdc3eda48306191a
tryoma <tryoma0326@gmail.com>
2023-04-29 12:23:40 +0900

    e

--- dist/controllers/line.js
+++ dist/controllers/line.js
@@ -32,7 +32,7 @@ dotenv.config();
 const line = __importStar(require("@line/bot-sdk"));
 const fs_1 = __importDefault(require("fs"));
 const sync_1 = require("csv-parse/sync");
-const file = fs_1.default.readFileSync('src/ito.csv').toString();
+const file = fs_1.default.readFileSync('src/haa.csv').toString();
 const records = (0, sync_1.parse)(file, { columns: false });
 const moment = require('moment');
 const currentTime = moment();
@@ -48,7 +48,7 @@ mongoose_1.default
 });
 const Schema = mongoose_1.default.Schema;
 const DataSchema = new Schema({
-    title: String,
+    titleId: String,
     uniqId: String,
     usedNumber: Array,
     startedOn: Date,
@@ -60,19 +60,16 @@ const config = {
 };
 const client = new line.Client(config);
 const lineEndpoint = async (req, res, next) => {
-    if (req.body.events.length === 0) {
-        return res.status(201);
-    }
     const event = req.body.events[0];
     if (event.type === 'message' && event.message.type === 'text') {
         if (event.message.text === '新規') {
-            const title = records[1 + Math.floor(Math.random() * (records.length - 1))][0];
+            const titleId = records[1 + Math.floor(Math.random() * (records.length - 1))][0];
             const uniqId = Math.floor(Math.random() * 101) +
                 '-' +
                 currentTime.format('YYYYMMDDHH');
             const newNum = 1 + Math.floor(Math.random() * 8);
             const saveData = {
-                title: title,
+                titleId: titleId,
                 uniqId: uniqId,
                 usedNumber: [newNum],
                 startedOn: new Date(),
@@ -83,39 +80,51 @@ const lineEndpoint = async (req, res, next) => {
                     console.log(err);
                 }
                 else {
-                    const title = Number(data.title);
+                    const titleId = Number(data.titleId);
+                    const selectRecord = records[titleId - 1];
                     client.replyMessage(event.replyToken, [
-                        textTemplate(`お題は 『${saveData.title}』 です。`),
-                        textTemplate(`お題IDは`),
-                        textTemplate(`${saveData.uniqId}`),
-                        textTemplate(`です。`),
+                        textTemplate(`あなたの番号は『${newNum}』`),
+                        textTemplate(`お題IDは『${data.uniqId}』`),
+                        textTemplate(`お題は『${selectRecord[1]}』\n1. ${selectRecord[2]}\n2. ${selectRecord[3]}\n3. ${selectRecord[4]}\n4. ${selectRecord[5]}\n5. ${selectRecord[6]}\n6. ${selectRecord[7]}\n7. ${selectRecord[8]}\n8. ${selectRecord[9]}`),
+                        textTemplate(`コピーしてメモにつかってください。\n1. \n2. \n3. \n4. \n5. \n6. \n7. \n8. `),
                     ]);
                 }
             });
+        }
+        else if (event.message.text === 'お題IDあり') {
+            client.replyMessage(event.replyToken, textTemplate('お題IDを貼り付けて下さい。'));
         }
         else if (isUniqId(event.message.text)) {
             const lineText = event.message.text;
             const data = await DataModel.findOne({ uniqId: event.message.text });
             if (data) {
                 const array = data.usedNumber;
-                let newNum = Math.floor(Math.random() * 101);
-                while (array.includes(newNum)) {
-                    newNum = Math.floor(Math.random() * 101);
+                if (array.length >= 8) {
+                    client.replyMessage(event.replyToken, textTemplate('人数は8人までです。'));
                 }
-                const newArray = [...array, newNum];
-                await DataModel.updateOne({ uniqId: lineText }, {
-                    $set: {
-                        usedNumber: newArray,
-                    },
-                });
-                client.replyMessage(event.replyToken, [
-                    textTemplate(`あなたの番号は『${data.title}』`),
-                    textTemplate(`お題は『${newNum}』`),
-                ]);
+                else {
+                    let newNum = Math.floor(1 + Math.random() * 8);
+                    while (array.includes(newNum)) {
+                        newNum = Math.floor(1 + Math.random() * 8);
+                    }
+                    const newArray = [...array, newNum];
+                    await DataModel.updateOne({ uniqId: lineText }, {
+                        $set: {
+                            usedNumber: newArray,
+                        },
+                    });
+                    const titleId = Number(data.titleId);
+                    const selectRecord = records[titleId - 1];
+                    client.replyMessage(event.replyToken, [
+                        textTemplate(`あなたの番号は『${newNum}』`),
+                        textTemplate(`お題は『${selectRecord[1]}』\n1. ${selectRecord[2]}\n2. ${selectRecord[3]}\n3. ${selectRecord[4]}\n4. ${selectRecord[5]}\n5. ${selectRecord[6]}\n6. ${selectRecord[7]}\n7. ${selectRecord[8]}\n8. ${selectRecord[9]}`),
+                        textTemplate(`コピーしてメモにつかってください。\n1. \n2. \n3. \n4. \n5. \n6. \n7. \n8. `),
+                    ]);
+                }
             }
         }
         else {
-            client.replyMessage(event.replyToken, textTemplate('『新規』 または、正しい『お題ID』を入力して下さい'));
+            client.replyMessage(event.replyToken, confirmTemplate());
         }
     }
     res.status(201);
